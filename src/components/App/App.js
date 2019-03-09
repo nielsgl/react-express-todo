60aef61b7a3f05df2cde6502db37869f4c53a2a1
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-09 17:45:18 +0100

    add edit guessers
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import './App.css';
@@ -10,8 +10,8 @@ class App extends React.Component {
 	render() {
 		return (
 			<Admin dataProvider={dataProvider}>
-				<Resource name="users" list={ListGuesser} />
-				<Resource name="todos" list={ListGuesser} />
+				<Resource name="users" list={ListGuesser} edit={EditGuesser} />
+				<Resource name="todos" list={ListGuesser} edit={EditGuesser} />
 			</Admin>
 		);
 	}
