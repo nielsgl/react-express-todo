3c4d3d872b50c029683cab8f833cadafe316e619
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-10 14:34:19 +0100

    update api.js

--- api/app.js
+++ api/app.js
@@ -2,19 +2,54 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+let cors = require('cors');
 
 const indexRouter = require('./routes/index');
+const tasksRouter = require('./routes/tasks');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
 app.use(logger('dev'));
+
+const corsOptions = {
+	exposedHeaders: ['X-Total-Count'],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/tasks', tasksRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500).json({
+			message: err.message,
+			error: err,
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+	res.status(err.status || 500).json({
+		message: err.message,
+		error: {},
+	});
+});
 
 module.exports = app;
