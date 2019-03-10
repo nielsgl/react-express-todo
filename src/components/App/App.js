f631ed83d644c352cb89cc03ef5431730093ed95
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-10 14:35:44 +0100

    update react app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,41 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import {
+	Admin,
+	Resource,
+	ListGuesser,
+	EditGuesser,
+	ShowGuesser,
+} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import './App.css';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+import { TaskCreate, TaskEdit, TaskList, TaskShow } from 'components/Tasks';
+import { UserCreate, UserEdit, UserList, UserShow } from 'components/Users';
+
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('http://localhost:5000');
 
 class App extends React.Component {
 	render() {
 		return (
 			<Admin dataProvider={dataProvider}>
-				<Resource name="users" list={ListGuesser} edit={EditGuesser} />
-				<Resource name="todos" list={ListGuesser} edit={EditGuesser} />
+				{/* <Resource name="users" list={ListGuesser} edit={EditGuesser} /> */}
+				<Resource
+					name="users"
+					create={UserCreate}
+					edit={UserEdit}
+					list={UserList}
+					show={UserShow}
+				/>
+				<Resource
+					name="tasks"
+					create={TaskCreate}
+					edit={TaskEdit}
+					list={TaskList}
+					show={TaskShow}
+				/>
+				{/* <Resource name="todos" list={ListGuesser} edit={EditGuesser} /> */}
 			</Admin>
 		);
 	}
