70446304a0ded977986f64304a4e21e29af37b23
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-10 14:36:31 +0100

    update task show component

---
+++ src/components/Tasks/TaskShow.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {
+	Show,
+	SimpleShowLayout,
+	TextField,
+	BooleanField,
+	NumberField,
+	DisabledInput,
+	ReferenceField,
+} from 'react-admin';
+
+const TaskShow = props => {
+	return (
+		<Show {...props}>
+			<SimpleShowLayout>
+				{/* <DisabledInput source="id" /> */}
+				<ReferenceField source="User.id" reference="users">
+					<TextField source="username" />
+				</ReferenceField>
+				<TextField source="title" />
+				<BooleanField source="isDone" />
+				{/* <NumberField source="User.id" /> */}
+			</SimpleShowLayout>
+		</Show>
+	);
+};
+
+export default TaskShow;
