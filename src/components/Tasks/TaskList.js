acef097a245e0a36068a3b92a871b8ad280ff361
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-10 14:36:19 +0100

    update task list component

---
+++ src/components/Tasks/TaskList.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {
+	List,
+	Datagrid,
+	TextField,
+	BooleanField,
+	NumberField,
+	ReferenceField,
+} from 'react-admin';
+
+const TaskList = props => {
+	return (
+		<List {...props}>
+			<Datagrid rowClick="show">
+				<ReferenceField source="User.id" reference="users">
+					<TextField source="username" />
+				</ReferenceField>
+
+				<TextField source="id" />
+				<TextField source="title" />
+				<BooleanField source="isDone" />
+				{/* <NumberField source="User.id" /> */}
+			</Datagrid>
+		</List>
+	);
+};
+
+export default TaskList;
